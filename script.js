110911bb2b988c89a1c16482bdbcb12bd2148d60
HildaKornman <84178454+HildaKornman@users.noreply.github.com>
2021-06-15 23:52:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("Hallo Winc Academy Studenten");
+// Dit is een grote som en een voorbeeld van een regel comment
+let multiplication = 1230941*1823794;
+console.log("Multiplication:", multiplication);
+/* Als je meerdere regels comment is doe je dit over het algemeen
+op deze manier met een blok comment; in de opdracht heb ik de 2 sommen hiertusen gezet en dan zie je alleen nog Hallo t/m Studenten als je 'm runt in node */
+let division = 637263 / 54;
+console.log("Division:", division);*/
